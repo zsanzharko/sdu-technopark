4b81e9337f9f50a5dd42f4e575e77c0c881ff812
Ilyas <190107005@stu.sdu.edu.kz>
2022-10-27 11:54:41 +0600

    Old header template

--- react-website/src/components/header/Header.jsx
+++ react-website/src/components/header/Header.jsx
@@ -1,46 +1,102 @@
 import React from "react";
-import './header.scss'
+import './header.scss';
 import './../buttons/google-button.scss';
 import { menu, admin_menu } from "./menu";
+import logo from './logo.png';
 
 import logoImage from '../../assets/images/logo/logo_120.png';
 
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav className="main-nav">
-            <div className="header-wrapper">
-                <Link className="header-logo" to="/">
-                    <img src={logoImage} alt="Logo TechnoPark" loading="lazy" />
-                </Link>
-                
-                    <ul>{menu.map((item, idx) => (
-                        <li key={`menu key ${idx}`}>
-                            <Link to={item.link}>{item.title}</Link>
-                        </li>))}
-                        <li>
-                            <div className="google-btn">
-                                <div className="google-icon-wrapper">
-                                    <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" />
-                                </div>
-                                <p className="btn-text"><b>Sign in with google</b></p>
-                            </div>
+        // Navbar
+        <nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
+            <div class="container-fluid">
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarNavDropdown"
+                    aria-controls="navbarNavDropdown"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <a class="navbar-brand mt-2 mt-lg-0" href="#">
+                        <img
+                            src={"logo"}
+                            height="30"
+                            alt="Logo"
+                            loading="lazy"
+                        />
+                    </a>
+
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">О нас</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Актуальное</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Сотрудничество</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Контакты</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">FAQ</a>
                         </li>
                     </ul>
-      
+                </div>
+
+                <div class="d-flex align-items-center">
+                    <a
+                        type="button"
+                        class="btn btn-outline-primary btn-outline-sdu"
+                    >
+                        Вход
+                    </a>
+
+                    <div class="dropdown">
+                        <a
+                            class="dropdown-toggle d-flex align-items-center hidden-arrow caret"
+                            href="#"
+                            id="navbarDropdownMenuAvatar"
+                            role="button"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                        >
+                            <img
+                                class="rounded-circle"
+                                height="30"
+                                alt="Blank"
+                                loading="lazy"
+                            />
+                        </a>
+
+                        <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuAvatar">
+                            <li>
+                                <a class="dropdown-item" href="#">Профиль</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">Настройки</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item">Выход</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </nav>
     )
 }
-
-function myFunction(x) {
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
 
 export default Header;
 
