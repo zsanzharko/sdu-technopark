3ccd0a71aaf5527d05db0b927eecd482c54a5edd
Yersul <75240367+Yersul@users.noreply.github.com>
2022-11-20 20:47:04 +0600

    did footer (#7)
    * did footer
    
    * Delete react-website/src/pages/About directory
    
    * use props to get text
    
    * did responsive
    
    * did footer, add callable telephone number
    
    Co-authored-by: Sanzhar <57348097+zsanzharko@users.noreply.github.com>

--- react-website/src/pages/Home/Home.jsx
+++ react-website/src/pages/Home/Home.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import BannerHeader from '../../components/banner/Banner';
 import Header from '../../components/header/Header';
-// styles
-import style from './home.scss'
-
-import image from '../../assets/images/history-icon-512.png'
-
-import testImage from '../../assets/images/software-image.jpg'
-
-import wheelImg from "../../assets/images/wheel.svg";
+import Footer from '../../components/footer/Footer';
+import "./home.scss"
+import people  from '../../assets/images/sdu-people.png';
 
 const Home = () => {
 
@@ -17,102 +12,64 @@ const Home = () => {
     return (
         <>
             <Header />
-
-            <BannerHeader />
-
-            <div className="banner-animation">
-                <div className="banner-wheel">
-                    <img className="banner-img" src={wheelImg} alt="wheel" />
-                    <button className="banner-arrow arrowDown">❮</button>
-                </div>
+            <div className='banner-header'>
+                <BannerHeader />
             </div>
 
-            <div className={'fact_place'}>
-                <div className={'fact_inner'}>
-                    <h3 className={'fact_main_title'}>Некоторые факты о нас</h3>
-                    <div className={'fact_card_place'}>
-                        <div className="fact_card">
-                            <div className={'title_place'}>
-                                <h4 className='title'>
-                                    Наша история
-                                </h4>
+            
 
-                            </div>
-                            <div className="image_place">
-                                <img src={image} alt='history' />
-                            </div>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                Mauris rhoncus aenean vel elit. Quis viverra nibh cras pulvinar
-                                mattis nunc sed blandit libero.
-                            </p>
+            <div className='large-container'>
+                <div className='second-container'>
+                    <div className='innerof-container'>
+                        <h1>Наша история</h1>
+                    </div>
+                </div>
+            <div className='first-container'>
+                <div className='story-container'>
+                    
+                        <div className='heading-container' >
+                            <img src={people} alt="" />
                         </div>
-                        <div className="fact_card">
-                            <div className={'title_place'}>
-                                <h4 className='title'>
-                                    Наше видение
-                                </h4>
-                            </div>
-                            <div className="image_place">
-                                <img src={image} alt='history' />
-                            </div>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                Mauris rhoncus aenean vel elit. Quis viverra nibh cras pulvinar
-                                mattis nunc sed blandit libero.
-                            </p>
+                        <div className='content'>
+                            <p>Во всех учебниках пишут, что история – это наука о прошлом. История – это записи о том, что происходило в мире, в нашей стране или в нашем родном городе много лет назад.</p>
                         </div>
-                        <div className="fact_card">
-                            <div className={'title_place'}>
-                                <h4 className='title'>
-                                    Наши возможности
-                                </h4>
-                            </div>
-                            <div className="image_place">
-                                <img src={image} alt='history' />
-                            </div>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                Mauris rhoncus aenean vel elit. Quis viverra nibh cras pulvinar
-                                mattis nunc sed blandit libero.
-                            </p>
-                        </div>
+
+                </div>
+
+                <div className='story-container'>
+                    <div className='heading-container '>
+                        <img src={people} alt="" />
+                    </div>
+                    <div className='content'>
+                        <p> Даже то, что происходит с нами сейчас, когда-то станет историей и попадет в школьные учебники.
+                            Знать историю своего народа также важно, как и разбираться в том, что происходило во всем мире давным-давно.</p>
+                    </div>
+                </div>
+                <div className='story-container'>
+                    <div className='heading-container '>
+                        <img src={people} alt="" />
+                    </div>
+                    <div className='content'>
+                        <p>История учит нас положительным и отрицательным примерам. Мы можем сделать выводы на ошибках прошлого и больше их не совершать. </p>
                     </div>
                 </div>
             </div>
-            <div className='direction_place'>
-                <div className="direction_place_inner">
-                    <div className="direction_place_split">
-                        <h1 className="direction_main_title">
-                            Наши направления и предложения
-                        </h1>
-                    </div>
-                    <div className="direction_place_split">
-                        <div className="direction_info">
-                            <div className="flex-info">
-                                <div className="direction_image_info">
-                                    <div className="direction_image_info_inner">
-                                        <h2>Text</h2>
-                                    </div>
-                                </div>
-                                <div className="direction_image_info">
-                                    <div className="direction_image_info_inner">
-                                        <h2>Text</h2>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="direction_image_info">
-                                <div className="direction_image_info_inner">
-                                    <h2>Text</h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            </div>
+            <div className='vision'>
+                <div className='vision-content-1'>
+
+                </div>
+                <div className='vision-content-2'>
+                    <h1>ВИДЕНИЕ И ВОЗМОЖНОСТИ</h1>
+                </div>
+                <div className='vision-content-3'>
                 </div>
             </div>
+
+            
+          
+
+            <Footer text = "Нужна помощь? Свяжитесь с нами:" number = "+7 (727) 307 95 65" link ="www.technopark.sdu.edu.kz" email = "technopark@sdu.edu.kz" />
         </>
     );
 }
