e4bc4bffac0d36b876eee89a938de641163f202f
Yersul <190107036@stu.sdu.edu.kz>
2022-11-26 02:16:48 +0600

    did contact page

--- react-website/src/pages/Contact/Contact.jsx
+++ react-website/src/pages/Contact/Contact.jsx
@@ -1,10 +1,86 @@
 import React from 'react';
 import Header from '../../components/header/Header';
+import './contact.scss'
+import Footer from '../../components/footer/Footer';
+import nazim from '../../assets/images/nazim.png';
+import book from '../../assets/images/book.png';
+import location from '../../assets/images/LocationIcon.svg';
+
+
+
+
 
 function Contact() {
+    const name = "Nazim Ibragimov"
+    const position  = 'Senior Lecturer'
+    const mobile = '8-702-586-98-75'
+
+    const block = "block 5, number 69"
+    const address = "Алматинская область, Kарасайский район. 040900, город Каскелен, ул. Абылай хана, 1/1"
+  
+    
+
+
     return (
         <>
             <Header />
+
+            <div className='largest-container'>
+                <div className='outer-container'>
+                    <div className='inner-container'>
+                        <h1>Контакты</h1>
+                    </div>
+                </div>
+            <div className='lecturer-container'>
+                <div className='contacts-container'>
+                    <div className='info-contact'> 
+                        <div className='drawing-line'>
+                            <div className='line'>
+                            <canvas id="myCanvas">
+</canvas>
+                            </div>
+                            <div className='heading-container' >
+                                <div className='photo-in'>
+                                    <img src={nazim} alt="" />
+                                </div>
+                                
+                            </div>
+                        
+                        <div className='content'>
+                            <p className='text'>{name}</p>
+                            <p>{position}</p>
+                            <p>{mobile}</p>
+
+                        </div>
+                    </div></div>
+                </div>
+
+                <div className='maps-container'>
+                    <div className='sdu-in-google'>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11632.333264440063!2d76.6690542!3d43.2077418!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7b7d14aec270c056!2sSDU%20(Suleyman%20Demirel%20University)!5e0!3m2!1sen!2skz!4v1669394791162!5m2!1sen!2skz" width="600" height="450" loading="lazy">grame body</iframe>
+                    </div>
+                    <div className='address-cont'>
+                        <div className='detail-container'>
+                            <div className='where-to-find'>
+                                <img src={book} alt="" />
+                                <p>{block}</p>
+                            </div>
+                            <div className='where-to-find'>
+                                <img src={book} alt="" />
+                                <p>{block}</p>
+                            </div>
+                        </div>
+                        <div className='detailed-address'>
+                            <img src={location} alt="" />
+                            <p>{address}</p>
+
+                        </div>
+                    </div>
+                </div>
+                
+                </div>
+            </div>
+            <Footer  />
         </>
     );
 }
