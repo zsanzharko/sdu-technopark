4eaa72375177827ee371f6455d589892c70b93f2
Yersul <190107036@stu.sdu.edu.kz>
2022-12-04 14:48:50 +0600

    did contact

--- react-website/src/pages/Contact/Contact.jsx
+++ react-website/src/pages/Contact/Contact.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from '../../components/header/Header';
 import './contact.scss'
 import Footer from '../../components/footer/Footer';
 import author from '../../assets/images/author.png';
