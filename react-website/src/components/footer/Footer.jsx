48666e632123eb2e754b5b45006831cb7538678b
Ilyas <190107005@stu.sdu.edu.kz>
2022-12-13 21:02:04 +0600

    footer fix

--- react-website/src/components/footer/Footer.jsx
+++ react-website/src/components/footer/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './footer.scss'
 import facebook from '../../assets/images/link-icons/facebook-icon/facebook.png';
 import sdulogo from '../../assets/images/sdu_technopark_logo.png'
@@ -7,9 +6,7 @@ import linkedin from '../../assets/images/link-icons/linkedin-icon/linkedin.png'
 import instagram from '../../assets/images/link-icons/instagram-icon/instagram.png';
 import earth from '../../assets/images/link-icons/earth-icon/earth.svg';
 import envelope from '../../assets/images/link-icons/envelope-icon/Envelope.svg';
-
-
-
+import {Link} from "react-router-dom";
 
 function Footer() {
     const number = '+7 (727) 307 95 65'
@@ -25,11 +22,16 @@ function Footer() {
                 <div className='technopark-logo'>
                     <img src={sdulogo} alt="Logo TechnoPark" loading="lazy" />
                 </div>
-                <div className='social-networks'>    
-                    <img src={facebook} alt="facebook" loading="lazy" />
-                    <img src={instagram} alt="instagram" loading="lazy" className='instagram' />
-                    <img src={linkedin} alt="linkedin" loading="lazy" />
-                    
+                <div className='social-networks'>
+                    <Link className="sn-link" to="/">
+                        <img src={facebook} alt="facebook" loading="lazy" />
+                    </Link>
+                    <Link className="sn-link" to="/">
+                        <img src={instagram} alt="instagram" loading="lazy"/>
+                    </Link>
+                    <Link className="sn-link" to="/">
+                        <img src={linkedin} alt="linkedin" loading="lazy" />
+                    </Link>
                 </div>
             </div>
             
