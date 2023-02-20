b046613d3efa50fd155a5cc4cd8f303497137c07
Sanzhar <57348097+zsanzharko@users.noreply.github.com>
2023-02-20 22:42:43 +0600

    Revert "Footer (#24)" (#29)
    This reverts commit e7930ff1eba138e367459297b097e0e5972648f9.

--- react-website/src/components/footer/Footer.jsx
+++ react-website/src/components/footer/Footer.jsx
@@ -1,58 +1,9 @@
 import React from 'react';
 
-import './footer.scss'
-import facebook from '../../assets/images/link-icons/facebook-icon/facebook.png';
-import sdulogo from '../../assets/images/sdu_technopark_logo.png'
-import linkedin from '../../assets/images/link-icons/linkedin-icon/linkedin.png';
-import instagram from '../../assets/images/link-icons/instagram-icon/instagram.png';
-import earth from '../../assets/images/link-icons/earth-icon/earth.svg';
-import envelope from '../../assets/images/link-icons/envelope-icon/Envelope.svg';
-
-
-
+import style from './footer.scss'
 
 function Footer() {
-    const number = '+7 (727) 307 95 65'
-    const name = 'Нужна помощь? Свяжитесь с нами:'
-    const link = 'www.technopark.sdu.edu.kz'
-    const email = 'technopark@sdu.edu.kz'
-
-
-    return ( <>
-    
-        <div className='footer'>
-            <div className='logos-banner'>
-                <div className='technopark-logo'>
-                    <img src={sdulogo} alt="Logo TechnoPark" loading="lazy" />
-                </div>
-                <div className='social-networks'>    
-                    <a href=""><img src={facebook} alt="facebook" loading="lazy" /></a>
-                    <a href="" className='first'><img src={instagram} alt="instagram" loading="lazy" /></a>
-                    <a href="" className='first'><img src={linkedin} alt="linkedin" loading="lazy" /></a>
-                    
-                </div>
-            </div>
-            
-            <div className='copyrights'>
-                <div className='need-help'>
-                    <h1>{name}</h1>
-                    <a href="tel: {number}">{number}</a>
-                </div>
-                <div className='uni-contacts'>
-                    <div className='contact'>
-                        <img src={earth} alt="website" loading="lazy" />
-                        <p>{link}</p>
-                    </div>
-                    <div className='contact'>
-                        <img src={envelope} alt="website" loading="lazy" />
-                        <p>{email}</p>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    
-    </> );
+    return ( <></> );
 }
 
 export default Footer;
